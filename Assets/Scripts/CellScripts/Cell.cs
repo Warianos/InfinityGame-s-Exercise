cb2431289176deaaaa0daf3684583b4740e260b9
Warianos <jox_nds@hotmail.com>
2019-02-06 13:12:38 +0000

    Gameplay
    All gameplay prefabs done.
    Possible to know if object is connected.
    Each different cell has it´s own behaviour
    -> Next will be Implementing the game manager to populate the grid

---
+++ Assets/Scripts/CellScripts/Cell.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+    private bool hasPower;
+
+    protected bool HasPower
+    {
+        get
+        {
+            return hasPower;
+        }
+
+        set
+        {
+            hasPower = value;
+        }
+    }
+
+    //public virtual void VerifyConnection() { }
+    public virtual void NoPowerColorChange() { }
+    public virtual void PowerColorChange() { }
+	
+	
+}
